0026727a4c1a632fb93885b058650833f05883ec
Fabian Kiprop <fabrotich@gmail.com>
2015-08-14 15:49:45 +0300

    Added a reurn type to the methods

--- system/Helpers/Cookie.php
+++ system/Helpers/Cookie.php
@@ -45,60 +45,96 @@ class Cookie
      * @param mixed $domain The domain that the cookie is available to
      * @param bool|string $secure Indicates whether the cookie is transmitted over a secure HTTPS connection from the client.
      * @param bool|string $httponly Indicates whether the cookie is accessible over the HTTP protocol
-     * @return void
+     * @return bool true | false Returns true if cookie is successfully set and false if not
      */
     public static function set($name, $value = "", $time = 3600, $path = "/", $domain = "", $secure = false, $httponly = true)
     {
         //Call native php setCookie function to set the cookies
-        setcookie($name, $value, time()+$time, $path, $domain, $secure, $httponly);
-        echo self::get($name);
+        setcookie($name, $value, time() + $time, $path, $domain, $secure, $httponly);
+
+        //Checks if cookie is set
+        if ( isset($_COOKIE[$name]) )
+        {
+
+            //Returns true if set
+            return true;
+        }
+
+        else
+        {
+
+            //Returns false if cookie failed to set
+            return false;
+        }
+
     }
 
     /**
      * This method returns cookie data by name
-     * @params string $value the value with which to search cookie data
-     * @param $name The name with which the cookie was stored
-     * @return mixed the value stored in the cookie
+     *
+     * @param string $name The name with which the cookie was stored
+     * @return mixed The value stored in the cookie or false if cookie not found
      */
     public static function get($name)
     {
-        //Check if cookie is set/exists
-        if (!isset($_COOKIE[$name])) {
-            //Return false if it doesn't exist
+        //Check if cookie is set
+        if ( ! isset($_COOKIE[$name]) )
+        {
+            //Return false if it cookie isn't set
             return false;
-        } else {
-            //return if the cookie is set/$exists is true
-            return $_COOKIE[$name];
+
         }
+
+        //Cookie is set so return the value
+        else
+        {
+            //Returns true
+            return $_COOKIE[$name];
+
+        }
+
     }
 
     /**
-     *This method deletes data from a cookie
+     *This method deletes data from a cookie by cookie name
      *
-     * @param $name
-     * @internal param string $value the value with which to delete cookie data
-     * @return void
+     * @param string $name The name of the cookie to delete
+     * @return boolean true | false True if cookie is successfully deleted and False if cookie is not
      */
     public static function delete($name)
     {
         //If a cookie exists delete it
-        if(isset($_COOKIE[$name])){
+        if (isset($_COOKIE[$name]))
+        {
             //Clear both $_COOKIE and browser cookie
-            setcookie($name, "", time()-3600, "/");
+            setcookie($name, "", time() - 3600, "/");
             unset($_COOKIE[$name]);
+            return true;
+
         }
+
+        //There is no cookie set
+        else{
+
+            //Returns false
+            return false;
+
+        }
+
     }
 
     /**
-     *This method checks if cookies are enabled
-     * @return boolean
+     *This method checks if cookies are enabled on the client browser
+     *
+     * @param NULL
+     * @return boolean true | false True if enabled and False if not enabled
      */
     public static function enabled()
     {
         //set a test cookie
         setcookie("test_cookie", "test", time() + 3600, '/');
 
-//        check if test cookie was stored
+        //check if test cookie was stored
         $enabled = (count($_COOKIE > 0)) ? true : false;
 
         return $enabled;
@@ -114,12 +150,31 @@ class Cookie
      * @param mixed $domain The domain that the cookie is available to
      * @param bool|string $secure Indicates whether the cookie is transmitted over a secure HTTPS connection from the client.
      * @param bool|string $httponly Indicates whether the cookie is accessible over the HTTP protocol
+     * @return boolean true | false True if cookie is successfully updated and False if not
      */
 
-    public static function update($name, $value, $time = 3600, $path = "/", $domain = "", $secure = false, $httponly = true){
+    public static function update($name, $value, $time = 3600, $path = "/", $domain = "", $secure = false, $httponly = true)
+    {
         //Calls the native php 'setcookie' method to update the cookie
-        setcookie($name, $value, time()+$time, $path, $domain,$secure, $httponly);
+        setcookie($name, $value, time() + $time, $path, $domain, $secure, $httponly);
+
+        //Checks if cookie is updated
+        if ( isset($_COOKIE[$name]) )
+        {
+
+            //Returns true if cookie is updated
+            return true;
+        }
+
+        else
+        {
+
+            //Returns false if cookie failed to update
+            return false;
+        }
+
     }
+
 
 
 }

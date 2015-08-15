15e6eb72aa2e58fc1953bc0cfbca3197c4a55db6
Fabian Kiprop <fabrotich@gmail.com>
2015-08-15 12:32:29 +0300

    Edited the test method, cookie in HomeController.php to test the Cookie class.

--- application/controllers/HomeController.php
+++ application/controllers/HomeController.php
@@ -32,17 +32,15 @@ class HomeController extends BaseController {
 	}
 
     /**
-     *This method loads tests the cookie class
+     *This method tests the cookie class
      *
      *@param null
      *@return void
      *@throws null This method doesn't throw any error.
      */
     public function cookie(){
-        $number = 22;
         Cookie::set("Home", "play");
         $cookieValue1 = Cookie::get("Home");
-//        $cookieValue2 = Cookie::get("Glivers");
         echo $cookieValue1;
     }
 
